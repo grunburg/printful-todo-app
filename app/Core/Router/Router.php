4f8fcf9a279214ed1119e9234da19d35eabe8535
Ričards <ricards.grinbergs@outlook.com>
2021-06-08 18:43:02 +0300

    Added POST request to Router

--- app/Core/Router/Router.php
+++ app/Core/Router/Router.php
@@ -10,10 +10,17 @@ class Router
        self::match('GET', $route, $fn);
     }
 
+    public static function post(string $route, callable $fn)
+    {
+        self::match('POST', $route, $fn);
+    }
+
     public static function match($method, $route, $fn)
     {
+        $request = explode('?', $_SERVER['REQUEST_URI']);
+
         if ($method === $_SERVER['REQUEST_METHOD']) {
-            if (preg_match(self::pattern($route), $_SERVER['REQUEST_URI'], $values)) {
+            if (preg_match(self::pattern($route), $request[0], $values)) {
                 call_user_func_array($fn, array_slice($values, 1));
                 return;
             }
