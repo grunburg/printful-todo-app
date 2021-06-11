9057fe58a82deb999535d6e9cbac4d2e96461f05
Ričards <ricards.grinbergs@outlook.com>
2021-06-11 15:16:24 +0300

    Added create table method to database

--- api/app/Core/Database/Database.php
+++ api/app/Core/Database/Database.php
@@ -13,6 +13,8 @@ class Database
     private string $username = 'root';
     private string $password = 'password';
 
+    private string $table = "create_tasks_table";
+
     private PDO $connection;
 
     public function __construct()
@@ -29,6 +31,17 @@ class Database
     public function connection(): PDO
     {
         return $this->connection;
+    }
+
+    public function create()
+    {
+        $query = 'SELECT 1 FROM tasks LIMIT 1';
+        $result = $this->connection()->query($query);
+
+        if ($result == false) {
+            $file = file_get_contents(ROOT . '/database/' . $this->table . '.sql');
+            $this->connection()->query($file);
+        }
     }
 
 }
