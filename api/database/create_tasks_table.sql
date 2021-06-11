315d175446b3b27a3242933df90fa2a29ac9f19e
Ričards <ricards.grinbergs@outlook.com>
2021-06-11 15:17:00 +0300

    Added table structure

--- api/database/create_tasks_table.sql
+++ api/database/create_tasks_table.sql
@@ -1,3 +1,8 @@
 CREATE TABLE tasks
 (
+    id          int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name        TINYTEXT                           NOT NULL,
+    description TEXT                               NULL     DEFAULT '',
+    completed   BOOLEAN                            NOT NULL DEFAULT 0,
+    created_at  TINYTEXT                           NULL     DEFAULT CURRENT_TIMESTAMP
 );
