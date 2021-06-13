7e96f2a99e0191686a4cdad8d0539fb0568efb34
Ričards <ricards.grinbergs@outlook.com>
2021-06-13 22:06:40 +0300

    Added headers to allow CORS for POST requests

--- api/public/index.php
+++ api/public/index.php
@@ -1,6 +1,15 @@
 <?php
 
 /*
+ * Headers to allow POST requests from FE to BE API
+ */
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+/*
  * Register Composer Auto Loader
  */
 
