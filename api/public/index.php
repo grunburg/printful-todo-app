2dcc32faab14a47ba0135a40e5095b1ef899dc59
Ričards <ricards.grinbergs@outlook.com>
2021-06-13 23:01:58 +0300

    Added header for CORS request

--- api/public/index.php
+++ api/public/index.php
@@ -4,6 +4,7 @@
  * Headers to allow POST requests from FE to BE API
  */
 
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
