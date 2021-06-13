7820e17e0cae552493d881aa1cec397bb9a69eae
Ričards <ricards.grinbergs@outlook.com>
2021-06-14 01:23:32 +0300

    Navigation now has specific icons per route

--- app/src/components/Navigation.vue
+++ app/src/components/Navigation.vue
@@ -1,9 +1,7 @@
 <template>
   <nav class="my-6 flex items-center">
     <button class="p-2 bg-gray-900 rounded-xl focus:outline-none" @click="send('/')">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-50" viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-      </svg>
+      <i v-html="setIcon"></i>
     </button>
     <span class="ml-2 font-medium text-xl text-gray-900">{{ setTitle }}</span>
   </nav>
@@ -15,7 +13,8 @@ export default {
 
   data() {
     return {
-      title: 'Menu'
+      title: 'Menu',
+      icon: null
     }
   },
 
@@ -29,6 +28,16 @@ export default {
         return this.title = 'Back'
       }
     },
+
+    setIcon() {
+      if (this.$route.name === 'Tasks') {
+        return this.icon = '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-50" viewBox="0 0 20 20" fill="currentColor"> <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" /> </svg>'
+      }
+
+      if (this.$route.name === 'Task') {
+        return this.icon = '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-50" viewBox="0 0 20 20" fill="currentColor"> <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" /> </svg>'
+      }
+    }
   },
 
   methods: {
